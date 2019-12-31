235b9fc725083a57c673490852d923e84b872567
Kwabena <kwabena.a92@gmail.com>
2019-12-31 16:58:32 +0100

    initial commit

---
+++ calculator/src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    /**
+     * Returns the sum of two integers.
+     * @param x first integer
+     * @param y second integer
+     * @return sum
+     */
+    public int add(int x, int y) {
+        return x + y;
+    }
+}

fbdaa98aacd8bc88e3292c5d1e74acc323df35d6
Kwabena <kwabena.a92@gmail.com>
2019-12-31 19:09:06 +0100

    Add throws exception

--- positivenegative/src/main/java/StringMultiplier.java
+++ positivenegative/src/main/java/StringMultiplier.java
@@ -1,11 +1,15 @@
 public class StringMultiplier {
     /**
-     * Returns a new string which consists of the input string repeated as many times as the multiplier.
+     * Returns a new string which consists of the input string repeated as many times as the multiplier. Throws
+     * IllegalArgumentException if multiplier is negative.
      * @param str
      * @param multiplier
      * @return
      */
     String multiplyString(String str, int multiplier) {
+        if (multiplier < 0) {
+            throw new IllegalArgumentException();
+        }
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < multiplier; i++) {
             sb.append(str);
